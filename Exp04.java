ca9ecfb1b6285c657ecb4fdec1d3ce0e6388adf2
Mikail Agbal <97645740+agbalmikail@users.noreply.github.com>
2023-03-26 23:19:50 +0200

    Update Exp04.java

--- Exp04.java
+++ Exp04.java
@@ -9,7 +9,7 @@ public class Exp04 {
         System.out.println("Give a year");
         int year = tara.nextInt();
 
-        boolean leapYear = (year % 4 == 0 && year % 400 == 0);
+        boolean leapYear = (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
 
         System.out.println("Is it leap year? " + leapYear);
 

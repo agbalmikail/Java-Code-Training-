728c0bd7e124332af053f975e142b0d3694bd461
Mikail Agbal <97645740+agbalmikail@users.noreply.github.com>
2023-03-29 22:57:13 +0200

    Update Exp04.java

--- Exp04.java
+++ Exp04.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class Exp04 {
     public static void main(String[] args) {
-        Scanner tara = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         System.out.println("Give a year");
-        int year = tara.nextInt();
+        int year = input.nextInt();
 
         boolean leapYear = (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
 
         System.out.println("Is it leap year? " + leapYear);
 
-        tara.close();
+        input.close();
     }
 }

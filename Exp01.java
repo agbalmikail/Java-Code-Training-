277f524dc5d765ecffbb03373d152a7cb99f8b9f
Mikail Agbal <97645740+agbalmikail@users.noreply.github.com>
2023-03-29 22:58:50 +0200

    Update Exp01.java

--- Exp01.java
+++ Exp01.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class Exp01{
     public static void main(String[] args) {
-        Scanner tara = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         System.out.println("Is this number pozitife or negative?");
-        double sayi = tara.nextDouble();
+        double sayi = input.nextDouble();
         if(sayi < 0){
             System.out.println("The Number is negative");
         }else{
             System.out.println("The Number is pozitive");
         }
-        tara.close();
+        input.close();
     }
 }
